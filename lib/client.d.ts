f8904424545631c8708862aba04860e3793a2a53
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-03-14 02:17:05 -0400

    Some stuff

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -1,6 +1,39 @@
 class Client {
 	constructor(token: String, xsrf: String): void;
+	_makeRequest(location: String, method: String, body: Object, headers: Object): Promise;
+	
 	// Any functions added via the 'client' folder should be added here
+	/**
+	 * @description This function is called when the client is ready
+	 */
+	onready(): void;
+	/**
+	 * @description A preloaded JSON object from the 'settings.get' function
+	 */
+	_settings: Object;
+	settings: {
+		get(callback?: (response: Object)=>{}): Promise<Object>;
+	}
+	thumbnail: {
+		/**
+		 * 
+		 * @param size A string of 2 numbers seperated by an 'x' (<width>x<height>)
+		 * @returns A URL to the image
+		 */
+		full(size: String, callback?: (response: String)=>{}): Promise<String>;
+		/**
+		 * 
+		 * @param size A string of 2 numbers seperated by an 'x' (<width>x<height>)
+		 * @returns A URL to the image
+		 */
+		bust(size: String, callback?: (response: String)=>{}): Promise<String>;
+		/**
+		 * 
+		 * @param size A string of 2 numbers seperated by an 'x' (<width>x<height>)
+		 * @returns A URL to the image
+		 */
+		headshot(size: String, callback?: (response: String)=>{}): Promise<String>;
+	}
 }
 
 export function login(token: String): Client;
--- test.js
+++ test.js
@@ -13,7 +13,10 @@ const Client = require('./');
 
 
 const client = await Client.login(process.env['token']);
-
+client.onready = async()=>{
+	const url = await client.thumbnail.headshot('420x420');
+	console.log(url);
+}
 
 
 })();
